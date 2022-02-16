cab3620d1c99e7fbf6b7ff25ba06bb64225dc749
Jacob <jclay34@hotmail.com>
2022-02-15 19:47:31 -0600

    Initial Commit

---
+++ Shared/PaperNoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  PaperNoteApp.swift
+//  Shared
+//
+//  Created by Jacob Clay on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct PaperNoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
